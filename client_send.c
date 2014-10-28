3dd8c40e494ec9bfce919caea781e1aa0bb2cd82
gurity <gurity@163.com>
2014-10-28 15:42:23 +0000

    Use argv.

--- client_send.c
+++ client_send.c
@@ -12,14 +12,16 @@
 #define SERVER_PORT 8042
 
 int main(int argc, char* argv[]) {
+    char* server_ip = argv[1];
+    short server_port = atoi(argv[2]);
     // create a socket
     int sock = socket(AF_INET, SOCK_STREAM, 0);
 
     // connect to recv server
     struct sockaddr_in server_addr;
     server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(SERVER_PORT);
-    inet_aton(SERVER_IP, &server_addr.sin_addr); 
+    server_addr.sin_port = htons(server_port);
+    inet_aton(server_ip, &server_addr.sin_addr); 
     int res = connect(sock, (struct sockaddr*)&server_addr, sizeof(server_addr));
     if (res < 0) {
         perror("connect error");
@@ -27,7 +29,7 @@ int main(int argc, char* argv[]) {
     } 
 
     // send data
-    write(sock, argv[1], strlen(argv[1]));
+    write(sock, argv[3], strlen(argv[3]));
 
     char buf[1024 + 1] = {0};
     int n = read(sock, buf, 1024);
